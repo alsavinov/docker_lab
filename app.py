617346e82fbbb4b2b91fa1f2f0b95c84bcb74bdd
alsavinov <alexei2203s3537@gmail.com>
2023-04-07 15:50:41 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Создаем массив из 10 случайных чисел от 0 до 1
+random_array = np.random.rand(10)
+
+print("Random array:", random_array)
+print("Sum of array elements:", np.sum(random_array))
